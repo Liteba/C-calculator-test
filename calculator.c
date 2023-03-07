334c24190bfc6a0667b595413bd294d3f36ed729
Oluwaseyi Akintola <120264911+AlexOluwaseyi@users.noreply.github.com>
2023-03-07 12:18:04 +0100

    Fixed operator input errors

--- calculator.c
+++ calculator.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
+
 int main()
 {
-    double num1, num2;
+    double num1 = 2.1, num2 = 1.6;
     char operator;
 
     printf("Enter first number: ");
     scanf("%lf", &num1);
     printf("Enter second number: ");
-    scanf("%lf", &num2);
+    scanf(" %lf", &num2);
+
     printf("Enter an Operator (+, -, *, /, %): ");
-    scanf("%c", &operator);
+    scanf(" %c", &operator);
 
     switch (operator)
     {
     case '+':
-        printf("%.2lf + %.2lf = .2lf",num1,num2,(num1 + num2));
+        printf("%.2lf + %.2lf = %.2lf", num1,num2,(num1 + num2));
         break;
     case '-':
-        printf("%.2lf - %.2lf = .2lf",num1,num2,(num1 - num2));
+        printf("%.2lf - %.2lf = %.2lf", num1,num2,(num1 - num2));
         break;
     case '*':
-        printf("%.2lf * %.2lf = .2lf",num1,num2,(num1 * num2));
+        printf("%.2lf * %.2lf = %.2lf", num1,num2,(num1 * num2));
         break;
     case '/':
         if (num2 != 0.0)
-        printf("%.2lf / %.2lf = .2lf",num1,num2,(num1 / num2));
+        printf("%.2lf / %.2lf = %.2lf", num1,num2,(num1 / num2));
         else
         printf("Divide by zero situation");
         break;
-    default:
-        printf("invalid operator");
-
-    }
+//    default:
+//        printf("invalid operator");
+    
+        }
     return 0;
 }
